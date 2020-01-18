6cc0818453d1ef35c6fc92eb52b3a115674c7683
conary <conary@hotmail.es>
2020-01-17 23:45:59 -0600

    Initial project structure

---
+++ src/containers/Dashboard.js
@@ -0,0 +1,10 @@
+import React from "react";
+import CalendarWrapper from "../components/CalendarWrapper";
+
+export default function Dashboard() {
+  return (
+    <div className="container">
+      <CalendarWrapper />
+    </div>
+  );
+}
